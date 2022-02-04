0aec1f42634180d0cac5465f53d3cd2ff71c9cb3
Icaro G. P. Salgado <icarosalgadogp@gmail.com>
2022-02-03 22:11:57 -0300

    tutorial-05

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,25 +1,24 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
 
 # The first part of the serializer class defines the fields that get serialized/deserialized.
 # The create() and update() methods define how fully fledged instances are created or modified when calling serializer.save()
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(view_name="snippet-highlight", format="html")
 
     class Meta:
         model = Snippet
-        fields = ["id", "owner", "title", "code", "linenos", "language", "style"]
+        fields = ["url", "id", "highlight", "owner", "title", "code", "linenos", "language", "style"]
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
-    )
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name="snippet-detail", read_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
